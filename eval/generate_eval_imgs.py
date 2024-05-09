7dd24944f09373ac638a6c582da9a386f16b67ed
lls <luisa.schwirten@gmail.com>
2024-05-09 22:11:39 +0200

    remove unnecessary code

--- eval/generate_eval_imgs.py
+++ eval/generate_eval_imgs.py
@@ -15,7 +15,7 @@ import cv2
 NEGATIVE_PROMPT="monochrome, lowres, unrealistic, worst quality, low quality, cartoon, painting, drawing, digital art, black and white"
 PROMPT = "streetscape, dense fog, high quality photography, Canon EOS R3, street, natural lighting, detailed shadows, 35 mm lens"
 
-def load_depth_map (depthmap_filepath, size = 512, shift_depth_maps = False):
+def load_depth_map (depthmap_filepath, size = 512):
     depth =  load_image(depthmap_filepath)
     size = size
     filepath = depthmap_filepath
@@ -38,14 +38,12 @@ def main(args):
     lora_path = args.lora_path
     prompt = PROMPT
     negative_prompt = NEGATIVE_PROMPT
-    shift_depth_maps = False
+
 
     if args.pos_prompt:
         prompt = args.pos_prompt
     if args.neg_prompt:
         negative_prompt = args.neg_prompt
-    if args.shift_depth_maps:
-        shift_depth_maps = True
 
 
     out_dir = os.path.join('data', os.path.basename(depth_maps_dir) + '_' + os.path.basename(lora_path) )
@@ -68,17 +66,14 @@ def main(args):
     for i in os.listdir(depth_maps_dir):
         
         depth_fpath = os.path.join(depth_maps_dir, i)
-        #print(depth_fpath)
 
-        depth = load_depth_map(depth_fpath, shift_depth_maps =  shift_depth_maps)
+        depth = load_depth_map(depth_fpath)
         output_image = pipe(prompt, depth, negative_prompt=negative_prompt, num_inference_steps=50).images[0]
 
         s_path = os.path.join(out_dir, i)
         output_image.save(s_path)
 
 
-
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -89,7 +84,6 @@ if __name__ == "__main__":
     
     parser.add_argument("--pos_prompt", type = str, help= "positive prompt", required = False)
     parser.add_argument("--neg_prompt", type = str, help= "negative prompt", required = False)
-    parser.add_argument("--shift_depth_maps", type = bool, help= "True if shift dataset depthmaps are used", required = False)
 
 
     args = parser.parse_args()
